98023b1425162e623422ee9040c2bcfa6cacc866
saillern <schwarzesaillern@gmail.com>
2024-06-17 00:33:27 +0900

    Move GlobalStyle to MainPage.js

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,6 +1,35 @@
 import React from "react";
 import { DisplayIssueHeader, DisplayIssueTable, TabPage } from "./IssuePage";
 import { MainPage, MainTab, MainSection } from "../molecules/MainPagePart";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body{
+    margin: 0;  
+  }
+  html{
+    font-family: Lato, "Lucida Grande", "Lucida Sans Unicode", Tahoma, sans-serif;
+    line-height: 1.5;
+    font-size: 14px;
+    padding: 0px;
+    margin: 0;
+  }
+  a{
+    color: rgb(3,102,214);
+    text-decration:none;
+  }
+
+  *,*:before,*:after {
+    box-sizing: border-box;
+  }
+  li{
+    list-style-type:none;
+  }
+  ul{
+    padding:0px;
+    margin:0px;
+  }
+`;
 
 let isIssuePage = true;
 let isPullRequestPage = false;
@@ -11,6 +40,7 @@ export default function BodySection() {
 
   return (
     <>
+      <GlobalStyle />
       <MainPage>
         <MainTab>
           <TabPage
