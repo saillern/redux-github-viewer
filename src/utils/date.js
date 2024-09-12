7251f6f547bb4aedb07967a45da80926dc074020
saillern <schwarzesaillern@gmail.com>
2024-09-12 19:29:50 +0900

    fixed date.js

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,5 @@
+//Fixed: ファイルをsrc/utils/などに移動(前回レビュー箇所)
+//Fixed: ファイル変更する(前回レビュー箇所)
 export function getDate(d = new Date()) {
   const month = String(d.getMonth() + 1).padStart(2, "0");
   const day = String(d.getDate()).padStart(2, "0");
